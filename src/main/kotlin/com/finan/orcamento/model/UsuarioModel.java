7aa4e87b004576925a00bf39e6076025ca13ad31
matheus-carrasco <matheuscarrasco02@gmail.com>
2024-03-23 13:53:52 -0300

    Criação dos construtores, getters and setters da classe UsuarioModel

--- src/main/kotlin/com/finan/orcamento/model/UsuarioModel.java
+++ src/main/kotlin/com/finan/orcamento/model/UsuarioModel.java
@@ -29,4 +29,36 @@ public class UsuarioModel implements Serializable {
     @JsonIgnore
     @OneToMany(mappedBy = "id")
     private List<OrcamentoModel> orcamentos = new ArrayList<>();
+
+    public UsuarioModel(){}
+
+    public UsuarioModel(Long id, String nomeUsuario, List<OrcamentoModel> orcamentos) {
+        this.id = id;
+        this.nomeUsuario = nomeUsuario;
+        this.orcamentos = orcamentos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public List<OrcamentoModel> getOrcamentos() {
+        return orcamentos;
+    }
+
+    public void setOrcamentos(List<OrcamentoModel> orcamentos) {
+        this.orcamentos = orcamentos;
+    }
 }
